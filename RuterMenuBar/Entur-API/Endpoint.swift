1612293ae6e8a6a498a85189cb91a5a868bcddc7
Julian Mella <julianmella2@gmail.com>
2026-03-30 23:20:19 +0200

    feat: Endpoint protocol and extension

---
+++ RuterMenuBar/Entur-API/Endpoint.swift
@@ -0,0 +1,20 @@
+//
+//  Endpoint.swift
+//  RuterMenuBar
+//
+//  Created by Julian on 30/03/2026.
+//
+
+protocol Endpoint {
+    var url: String { get }
+    var etClientHeader: String { get }
+    var clientName: String { get }
+}
+
+// Entur API requires identification through the header ET-Client
+extension Endpoint {
+    var etClientHeader: String {"ET-Client-Header"}
+    var clientName: String {"julianmella-rutermenubar"}
+}
+
+
