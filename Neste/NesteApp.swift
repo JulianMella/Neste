cccdd60f54aa64d92892281b31179d035a4a6f09
Julian Mella <julianmella2@gmail.com>
2026-03-31 04:05:08 +0200

    feat: fetch data from entur and decode it with GeocoderModels

--- Neste/NesteApp.swift
+++ Neste/NesteApp.swift
@@ -15,7 +15,11 @@ struct NesteApp: App {
     var body: some Scene {
         MenuBarExtra("Test", systemImage: "tram.fill") {
             VStack {
-                Button(action: FetchOneHardcodedStop){
+                Button(action: {
+                    Task {
+                        await FetchBislettOsloHardcoded()
+                    }
+                }){
                     Text("Fetch stop")
                 }
             }
@@ -24,6 +28,17 @@ struct NesteApp: App {
     }
 }
 
-func FetchOneHardcodedStop() {
-    print("To be implemented")
+func FetchBislettOsloHardcoded() async {
+    let url = URL(string: "https://api.entur.io/geocoder/v1/autocomplete?text=Bislett, Oslo")!
+
+    var request = URLRequest(url: url)
+    request.setValue("julianmella-neste", forHTTPHeaderField: "ET-Client-Name")
+
+    do {
+        let (data, _) = try await URLSession.shared.data(for: request)
+        let decoded = try JSONDecoder().decode(GeocoderResponse.self, from: data)
+        print(decoded)
+    } catch {
+        print("Fetch error:", error)
+    }
 }
