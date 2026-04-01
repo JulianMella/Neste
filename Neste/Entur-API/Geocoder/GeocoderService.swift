4419e335e90b469d56b868d74ef74972563b5b83
Julian Mella <julianmella2@gmail.com>
2026-04-01 16:59:49 +0200

    feat: GeocoderService makes query to Entur

--- Neste/Entur-API/Geocoder/GeocoderService.swift
+++ Neste/Entur-API/Geocoder/GeocoderService.swift
@@ -5,9 +5,23 @@
 //  Created by Julian on 01/04/2026.
 //
 
+import Foundation
+
+struct GeocoderAutocomplete: Endpoint {
+    var url: String
+}
+
 final class GeocoderService {
+    let geocoderEndpoint = GeocoderAutocomplete(url: "https://api.entur.io/geocoder/v1/autocomplete?text=")
+    
     func autocomplete(query: String) async throws -> [Feature]? {
-        print("To be implemented")
+        do {
+            let (data, _) = try await URLSession.shared.data(for: geocoderEndpoint.makeRequest(query))
+            let decoded = try JSONDecoder().decode(GeocoderResponse.self, from: data)
+            print(decoded)
+        } catch {
+            print("Fetch error:", error)
+        }
         return nil
     }
 }
