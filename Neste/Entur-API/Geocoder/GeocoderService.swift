8cfef3236efc3498d76745442005c7ab47fae426
Julian Mella <julianmella2@gmail.com>
2026-04-01 16:45:01 +0200

    feat: Geocoder directory with skeleton code w.i.p

---
+++ Neste/Entur-API/Geocoder/GeocoderService.swift
@@ -0,0 +1,13 @@
+//
+//  GeocoderService.swift
+//  Neste
+//
+//  Created by Julian on 01/04/2026.
+//
+
+final class GeocoderService {
+    func autocomplete(query: String) async throws -> [Feature]? {
+        print("To be implemented")
+        return nil
+    }
+}
