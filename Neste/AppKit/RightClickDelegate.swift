571c01087848d0a342a2e1b0929eae862b287415
Julian Mella <julianmella2@gmail.com>
2026-04-01 16:36:43 +0200

    feat: comments, localized string and more safe if-checking

--- Neste/AppKit/RightClickDelegate.swift
+++ Neste/AppKit/RightClickDelegate.swift
@@ -14,7 +14,8 @@ class RightClickDelegate: NSObject, NSApplicationDelegate {
     }
     
     private func handleRightClick(_ event: NSEvent) -> NSEvent? {
-        if let window = event.window, window.className.contains("NSStatusBar") {
+        // Make sure that the event only goes through for a window of type statusBar
+        if let window = event.window, window.level == .statusBar {
             self.showMenu(in: window, at: event.locationInWindow)
         }
         return event
@@ -22,16 +23,18 @@ class RightClickDelegate: NSObject, NSApplicationDelegate {
     
     private func showMenu(in window: NSWindow, at point: NSPoint) {
         // Close the MenuBarExtra window if open
-        for w in NSApp.windows where w.isVisible && w != window {
+        // w.level == .popUpMenu is a bit brittle. If it ends up closing other windows such as Settings, it must be changed.
+        for w in NSApp.windows where w.isVisible && w != window && w.level == .popUpMenu {
             w.close()
         }
         
         let menu = NSMenu()
-        let quitItem = NSMenuItem(title: "Quit", action: #selector(quitApp), keyEquivalent: "")
+        let quitItem = NSMenuItem(title: NSLocalizedString("Quit", comment: "Quit the application"), action: #selector(quitApp), keyEquivalent: "")
         menu.addItem(quitItem)
         menu.popUp(positioning: nil, at: NSPoint(x: point.x, y: point.y), in: window.contentView)
     }
     
+    // NSApp.terminate can be directly called from the selector in quitItem. The issue with this is that, for some reason, it forces the Quit button to have an x icon next to it. This was the simplest way to get rid of that icon. Kinda hacky, weird and cool..
     @objc func quitApp() {
         NSApp.terminate(nil)
     }
