510cf784992d1bd4ce070767ef3b1de4f542ba30
Julian Mella <julianmella2@gmail.com>
2026-04-03 22:44:59 +0200

    fix: remove one upcoming arrival from list to avoid UI issues

--- Neste/Models/MockData.swift
+++ Neste/Models/MockData.swift
@@ -14,25 +14,45 @@ extension FavoriteStopsModel {
                     publicTransportNumber: "21",
                     transportType: .bus,
                     finalDestination: "Tjuvholmen",
-                    upcomingArrivals: []
+                    upcomingArrivals: [
+                        "5 min",
+                        "8 min",
+                        "14:15",
+                        "18:16",
+                    ]
                 ),
                 FavoriteStop.Line(
                     publicTransportNumber: "21",
                     transportType: .bus,
                     finalDestination: "Helsfyr",
-                    upcomingArrivals: []
+                    upcomingArrivals: [
+                        "Nå",
+                        "3 min",
+                        "12 min",
+                        "13:12",
+                    ]
                 ),
                 FavoriteStop.Line(
                     publicTransportNumber: "17",
                     transportType: .tram,
                     finalDestination: "Rikshospitalet",
-                    upcomingArrivals: []
+                    upcomingArrivals: [
+                        "5 min",
+                        "8 min",
+                        "9:15",
+                        "11:16",
+                    ]
                 ),
                 FavoriteStop.Line(
                     publicTransportNumber: "17",
                     transportType: .tram,
                     finalDestination: "Sinsen-Grefsen",
-                    upcomingArrivals: []
+                    upcomingArrivals: [
+                        "11 min",
+                        "13:14",
+                        "15:16",
+                        "18:20",
+                    ]
                 )
             ]
         )
