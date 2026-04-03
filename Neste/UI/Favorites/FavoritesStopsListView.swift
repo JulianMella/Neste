3c0541313fa6b393f17bca25e2454ee87ef10493
Julian Mella <julianmella2@gmail.com>
2026-04-03 20:31:58 +0200

    feat: ScrollView with a bunch of FavortieStopView

--- Neste/UI/Favorites/FavoritesStopsListView.swift
+++ Neste/UI/Favorites/FavoritesStopsListView.swift
@@ -5,45 +5,22 @@
 //  Created by Julian on 03/04/2026.
 //
 
+/**
+ * Abstract: Displays live public transportation stop data fetched from its Observable viewmodel
+ */
+
 import SwiftUI
 
-struct FavoriteStopView: View {
+struct FavoriteStopsListView: View {
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 8) {
-            HStack {
-                Text("12")
-                    .font(.system(size: 16))
-                    .bold()
-                    .frame(width: 32, height: 28)
-                    .background(.blue)
-                    .cornerRadius(8)
-                Text("Majorstuen")
-                    .font(.system(size: 16))
-                Spacer(minLength: 24)
-                Text("Nå")
-                    .font(.system(size: 16))
-                    .bold()
-            }
-            Divider()
-            HStack {
-                Text("2 min")
-                    .foregroundStyle(.gray)
-                Text("5 min")
-                    .foregroundStyle(.gray)
-                Text("12 min")
-                    .foregroundStyle(.gray)
-                Text("13:12")
-                    .foregroundStyle(.gray)
+        ScrollView {
+            VStack {
+                FavoriteStopView()
+                FavoriteStopView()
+                FavoriteStopView()
+                FavoriteStopView()
             }
         }
-        .padding()
-        .frame(width: 300)
     }
 }
-
-
-#Preview {
-    FavoritesView()
-
-}
-
