884908246628289128adeb8b80a7dd0b87d6dae3
Julian Mella <julianmella2@gmail.com>
2026-04-04 02:28:24 +0200

    feat: nested GeocoderResponse for global namespace protection

--- Neste/Entur/Geocoder/GeocoderModels.swift
+++ Neste/Entur/Geocoder/GeocoderModels.swift
@@ -5,15 +5,26 @@
 //  Created by Julian on 31/03/2026.
 //
 
+// A lot of the data from the JSON response is omitted as we do not care about it.
+// The Geometry key-value pair in Properties will be relevant if map functionality is implemented in the future
+
+
 import Foundation
 
 struct GeocoderResponse: Decodable {
     var features: [Feature]
-}
-
-struct Feature: Decodable {
-    var properties: Properties
     
+    struct Feature: Decodable {
+        var properties: Properties
+        
+        struct Properties: Decodable {
+            // var geometry: Geometry
+            var id: String
+            var name: String
+            var county: String
+            var category: [String]
+        }
+    }
 }
 struct Properties: Decodable {
     // var geometry: Geometry
