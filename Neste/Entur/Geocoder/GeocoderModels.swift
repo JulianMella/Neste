efd9f4d877e24b881f712161695e1f3bc68702b9
Julian Mella <julianmella2@gmail.com>
2026-04-04 02:28:54 +0200

    feat: readable GeocoderStop struct to be further parsed by ViewModels

--- Neste/Entur/Geocoder/GeocoderModels.swift
+++ Neste/Entur/Geocoder/GeocoderModels.swift
@@ -26,13 +26,15 @@ struct GeocoderResponse: Decodable {
         }
     }
 }
-struct Properties: Decodable {
-    // var geometry: Geometry
+
+struct GeocoderStop {
     var id: String
     var name: String
     var county: String
-    var category: [String]
-}
 
-// A lot of the data from the JSON response is omitted as we do not care about it.
-// The Geometry key-value pair in Properties will be relevant if map functionality is implemented in the future
+    // Category from GeocoderResponse.Feature.Properties is remapped to a set
+    // That specific field provides a list of all the public transportation types that exist in a given stop
+    // So it could be repetitive, saying [Bus, Bus, Tram, Tram] if many different lines travel through that stop
+    // This datapoint is only utilized in AddFavoritesView, and there we only need unique values.
+    var uniqueCategories: Set<String>
+}
