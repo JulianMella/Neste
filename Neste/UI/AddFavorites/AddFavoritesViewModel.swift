08413fe3f8fac1f6fc63e5bf88f953b25a18834e
Julian Mella <julianmella2@gmail.com>
2026-04-04 03:48:44 +0200

    feat: AddFavoritesViewModel which communicates with geocoder service

--- Neste/UI/AddFavorites/AddFavoritesViewModel.swift
+++ Neste/UI/AddFavorites/AddFavoritesViewModel.swift
@@ -8,14 +8,48 @@
 import Observation
 
 @Observable class AddFavoritesViewModel {
+    var errorMessage: String
+    var geocoderResults: [GeocoderStop]
+    var isLoading: Bool
     
-    var isLoading: Bool = false
     private let geocoderService: GeocoderService
     
     init() {
+        self.errorMessage = ""
+        self.geocoderResults = []
+        self.isLoading = false
         self.geocoderService = GeocoderService()
     }
     
+    func search(_ query: String) async {
+        do {
+            isLoading = true
+            
+            defer {
+                isLoading = false
+            }
+            
+            let stops: [GeocoderStop] = try await geocoderService.autocomplete(query: query)
+            
+            print(stops)
+            
+            geocoderResults = filter(stops)
+            
+            print(geocoderResults)
+            
+            
+        } catch EndpointError.networkError(let URLError) {
+            // TODO: Handle this
+        } catch EndpointError.httpError(let statusCode) {
+            // TODO: Handle this
+        } catch EndpointError.decodingError {
+            // TODO: Handle this
+        } catch {
+            // TODO: Handle this
+        }
+    }
     
-    
+    private func filter(_ stops: [GeocoderStop]) -> [GeocoderStop] {
+        return stops.filter { $0.county == "Oslo" && $0.id.hasPrefix("NSR:StopPlace")}
+    }
 }
