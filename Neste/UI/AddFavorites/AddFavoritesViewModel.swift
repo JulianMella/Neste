adef70fca430c5661c4f6b01222d6844c1a498b0
Julian Mella <julianmella2@gmail.com>
2026-04-05 21:08:40 +0200

    feat: geocoder -> stopPlace -> journeyPlanner API callsgit add -p Neste/UI/AddFavorites/AddFavoritesViewModel.swift

--- Neste/UI/AddFavorites/AddFavoritesViewModel.swift
+++ Neste/UI/AddFavorites/AddFavoritesViewModel.swift
@@ -9,29 +9,54 @@ import Observation
 
 @Observable class AddFavoritesViewModel {
     var errorMessage: String
-    var geocoderResults: [GeocoderStop]
+    var searchResults: [AddFavoritesResult]
     var isLoading: Bool
     
     private let geocoderService: GeocoderService
+    private let stopPlaceService: StopPlaceService
+    private let journeyPlannerService: JourneyPlannerService
     
     init() {
         self.errorMessage = ""
-        self.geocoderResults = []
+        self.searchResults = []
         self.isLoading = false
         self.geocoderService = GeocoderService()
+        self.stopPlaceService = StopPlaceService()
+        self.journeyPlannerService = JourneyPlannerService()
     }
     
     func search(_ query: String) async {
         do {
+            var queryResults: [AddFavoritesResult] = []
+
             isLoading = true
             
             defer {
                 isLoading = false
             }
             
-            let stops: [GeocoderStop] = try await geocoderService.autocomplete(query: query)
-            
-            geocoderResults = osloFilter(stops)
+            var stops: [GeocoderStop] = try await geocoderService.autocomplete(query: query)
+
+            stops = osloFilter(stops)
+
+            for stop in stops {
+                let children = try await stopPlaceService.fetchChildren(parentID: stop.id)
+                let hasChildren = !children.isEmpty
+                let stopIDs = hasChildren ? children.map{$0.id} : [stop.id]
+
+                let stopMetadata = try await fetchStopMetadata(for: stopIDs)
+
+                queryResults.append(
+                    AddFavoritesResult(
+                        parentStop: stop,
+                        hasChildren: hasChildren,
+                        stopMetadata: stopMetadata
+                    )
+                )
+            }
+
+            searchResults = queryResults
+
         } catch EndpointError.networkError(let URLError) {
             // TODO: Handle this
         } catch EndpointError.httpError(let statusCode) {
@@ -45,5 +70,26 @@ import Observation
     
     private func osloFilter(_ stops: [GeocoderStop]) -> [GeocoderStop] {
         return stops.filter { $0.county == "Oslo" && $0.id.hasPrefix("NSR:StopPlace")}
+    }
+
+    private func fetchStopMetadata(for stopIDs: [String]) async throws -> [AddFavoritesResult.StopMetadata] {
+        var metadata: [AddFavoritesResult.StopMetadata] = []
+        print("in")
+        for id in stopIDs {
+            let departures = try await journeyPlannerService.fetchStopData(stopPlaceID: id)
+            print("problem")
+            for departure in departures {
+                metadata.append(
+                    AddFavoritesResult.StopMetadata(
+                        id: id,
+                        transportType: departure.transportType,
+                        finalDestination: departure.frontText,
+                        publicTransportNumber: departure.publicCode
+                    )
+                )
+            }
+        }
+
+        return metadata
     }
 }
