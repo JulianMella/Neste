d86f0dc7d480c6b69355cd3e56be546e532d5c44
Julian Mella <julianmella2@gmail.com>
2026-04-05 21:04:49 +0200

    feat: initializer based upon queryType due to different strings from different endpoints zzz

--- Neste/Models/TransportType.swift
+++ Neste/Models/TransportType.swift
@@ -10,12 +10,24 @@ import SwiftUI
 enum TransportType {
     case bus, tram, metro // TODO: Add boat, train, green bus
     
-    init?(_ string: String) {
-        switch string {
-        case "onstreetBus": self = .bus
-        case "onstreetTram": self = .tram
-        case "metroStation": self = .metro
-        default: return nil
+    init?(_ string: String, queryType: QueryType) {
+        if queryType == .geocoder {
+            switch string {
+            case "onstreetBus": self = .bus
+            case "onstreetTram": self = .tram
+            case "metroStation": self = .metro
+            default: return nil
+            }
+        }
+        else if queryType == .journeyPlanner {
+            switch string {
+            case "bus": self = .bus
+            case "tram": self = .tram
+            case "metro": self = .metro
+            default: return nil
+            }
+        } else {
+            return nil
         }
     }
     
@@ -34,4 +46,8 @@ enum TransportType {
         case .metro: return "train.side.front.car"
         }
     }
+}
+
+enum QueryType {
+    case geocoder, journeyPlanner
 }
