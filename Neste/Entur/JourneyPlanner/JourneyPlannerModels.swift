245bace263cf4b87f1ade19499219c0beea12b46
Julian Mella <julianmella2@gmail.com>
2026-04-05 04:40:49 +0200

    feat: JourneyPlanner service and models

--- Neste/Entur/JourneyPlanner/JourneyPlannerModels.swift
+++ Neste/Entur/JourneyPlanner/JourneyPlannerModels.swift
@@ -1,7 +1,46 @@
 //
-//  JourneyPlannerService.swift
+//  JourneyPlannerModels.swift
 //  Neste
 //
 //  Created by Julian on 05/04/2026.
 //
 
+import Foundation
+
+struct GraphQLData<T: Decodable>: Decodable {
+    var data: T
+}
+
+struct StopPlaceData: Decodable {
+    var stopPlace: StopPlace
+    
+    struct StopPlace: Decodable {
+        var estimatedCalls: [EstimatedCall]
+        
+        struct EstimatedCall: Decodable {
+            var destinationDisplay: DestinationDisplay
+            var serviceJourney: ServiceJourney
+            
+            struct DestinationDisplay: Decodable {
+                var frontText: String
+            }
+            
+            struct ServiceJourney: Decodable {
+                var journeyPattern: JourneyPattern
+                
+                struct JourneyPattern: Decodable {
+                    var line: Line
+                    
+                    struct Line: Decodable {
+                        var publicCode: String
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct StopPlace: Hashable {
+    var publicCode: String
+    var frontText: String
+}
---
+++ Neste/Entur/JourneyPlanner/JourneyPlannerService.swift
@@ -0,0 +1,66 @@
+//
+//  JourneyPlannerService.swift
+//  Neste
+//
+//  Created by Julian on 05/04/2026.
+//
+
+import Foundation
+
+struct JourneyPlanner: GraphQLEndpoint {
+    var baseUrl: String = "https://api.entur.io/journey-planner/v3/graphql"
+}
+
+final class JourneyPlannerService {
+    let journeyPlanner = JourneyPlanner()
+    
+    func fetchStopData(stopPlaceID: String) async throws -> [StopPlace] {
+        let query = """
+            {
+              stopPlace(id: "\(stopPlaceID)") {
+                estimatedCalls(
+                    numberOfDeparturesPerLineAndDestinationDisplay: 1
+                  numberOfDepartures: 100
+                  whiteListed: {authorities: ["RUT:Authority:RUT"]}
+                ) {
+                  destinationDisplay {
+                    frontText
+                  }
+                  serviceJourney {
+                    journeyPattern {
+                      line {
+                        publicCode
+                      }
+                    }
+                  }
+                }
+              }
+            }
+            """
+
+        let (data, response): (Data, URLResponse)
+        
+        do {
+            (data, response) = try await URLSession.shared.data(for: journeyPlanner.makeRequest(with: query))
+        } catch {
+            throw EndpointError.networkError(error)
+        }
+
+        try journeyPlanner.validateHTTPResponse(response)
+        
+        let decoded: GraphQLData<StopPlaceData>
+        
+        do {
+            decoded = try JSONDecoder().decode(GraphQLData<StopPlaceData>.self, from: data)
+        } catch {
+            throw EndpointError.decodingError(error)
+        }
+        
+        return decoded.data.stopPlace.estimatedCalls.map {
+            StopPlace(
+                publicCode: $0.serviceJourney.journeyPattern.line.publicCode,
+                frontText: $0.destinationDisplay.frontText
+            )
+        }
+    }
+}
