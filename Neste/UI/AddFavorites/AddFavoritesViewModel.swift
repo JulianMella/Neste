ba2106066ee44001b2f059187b057951cfcb2aea
Julian Mella <julianmella2@gmail.com>
2026-04-07 15:43:47 +0200

    refactor: removed debug prints and conform to hasChildrenIds

--- Neste/UI/AddFavorites/AddFavoritesViewModel.swift
+++ Neste/UI/AddFavorites/AddFavoritesViewModel.swift
@@ -41,15 +41,15 @@ import Observation
 
             for stop in stops {
                 let children = try await stopPlaceService.fetchChildren(parentID: stop.id)
-                let hasChildren = !children.isEmpty
-                let stopIDs = hasChildren ? children.map{$0.id} : [stop.id]
+                let hasChildrenIds = !children.isEmpty
+                let stopIDs = hasChildrenIds ? children.map{$0.id} : [stop.id]
 
                 let stopMetadata = try await fetchStopMetadata(for: stopIDs)
 
                 queryResults.append(
                     AddFavoritesResult(
                         parentStop: stop,
-                        hasChildren: hasChildren,
+                        hasChildrenIds: hasChildrenIds,
                         stopMetadata: stopMetadata
                     )
                 )
@@ -74,10 +74,10 @@ import Observation
 
     private func fetchStopMetadata(for stopIDs: [String]) async throws -> [AddFavoritesResult.StopMetadata] {
         var metadata: [AddFavoritesResult.StopMetadata] = []
-        print("in")
+        
         for id in stopIDs {
             let departures = try await journeyPlannerService.fetchStopData(stopPlaceID: id)
-            print("problem")
+            
             for departure in departures {
                 metadata.append(
                     AddFavoritesResult.StopMetadata(
