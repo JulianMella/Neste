df97f00ec0a96c6abc9af3460f569bea93b8b12b
Julian Mella <julianmella2@gmail.com>
2026-04-07 15:49:26 +0200

    feat: entire rrefactor of favoritestopviewmodel to use list instead of dict and skeleton code for fetching data

--- Neste/UI/Favorites/FavoriteStopViewModel.swift
+++ Neste/UI/Favorites/FavoriteStopViewModel.swift
@@ -9,30 +9,108 @@ import Observation
 
 @Observable
 final class FavoriteStopViewModel {
-    var favoritedStops: [GeocoderStop : [AddFavoritesResult.StopMetadata]] = [:] // TODO: Persistent!
+    var favoritedStops: [FavoriteStop] = [] // TODO: Persistent!
+    var arrivalData: [String : [JourneyPlannerArrivalData]] = [:]
+    private let journeyPlannerService = JourneyPlannerService()
     
+    private func index(of parent: GeocoderStop) -> Int? {
+        favoritedStops.firstIndex(where: { $0.parentStop == parent })
+    }
     
-    func addFavorite(parent: GeocoderStop, child: AddFavoritesResult.StopMetadata) {
-        favoritedStops[parent, default: []].append(child)
+    private func index(of child: AddFavoritesResult.StopMetadata, in parentIndex: Int) -> Int? {
+        favoritedStops[parentIndex].stopMetadata.firstIndex(where: { $0 == child })
+    }
+    
+    func addFavorite(parent: GeocoderStop, hasChildrenIds: Bool, child: AddFavoritesResult.StopMetadata) {
+        if let parentIndex = index(of: parent) {
+            favoritedStops[parentIndex].stopMetadata.append(child)
+        } else {
+            favoritedStops.append(FavoriteStop(parentStop: parent, hasChildrenIds: hasChildrenIds, stopMetadata: [child]))
+        }
     }
     
     func deleteFavorite(parent: GeocoderStop, child: AddFavoritesResult.StopMetadata) {
-        favoritedStops[parent]?.removeAll { $0 == child }
+        if let parentIndex = index(of: parent) {
+            if let childIndex = index(of: child, in: parentIndex) {
+                favoritedStops[parentIndex].stopMetadata.remove(at: childIndex)
+            }
             
-        if favoritedStops[parent]?.isEmpty == true {
-            favoritedStops.removeValue(forKey: parent)
+            if favoritedStops[parentIndex].stopMetadata.isEmpty {
+                favoritedStops.remove(at: parentIndex)
+            }
         }
     }
     
     func deleteParent(parent: GeocoderStop) {
-        favoritedStops.removeValue(forKey: parent)
+        if let parentIndex = index(of: parent) {
+            favoritedStops.remove(at: parentIndex)
+        }
     }
     
-    func contains(parent: GeocoderStop, child: AddFavoritesResult.StopMetadata) -> Bool {
-        return favoritedStops[parent]?.contains(child) ?? false
+    func contains(child: AddFavoritesResult.StopMetadata, in parent: GeocoderStop) -> Bool {
+        guard let parentIndex = index(of: parent) else { return false }
+        
+        return favoritedStops[parentIndex].stopMetadata.contains{ $0 == child }
     }
     
     func getChildrenOf(_ parent: GeocoderStop) -> [AddFavoritesResult.StopMetadata] {
-        return favoritedStops[parent] ?? []
+        guard let parentIndex = index(of: parent) else { return [] }
+        
+        return favoritedStops[parentIndex].stopMetadata
     }
+    }
+    
+    func fetchArrivalData(for child: AddFavoritesResult.StopMetadata) async {
+        do {
+            /*isLoading = true TODO: Create isLoading array for each individual item that can be loaded.
+             
+             defer {
+             isLoading = false
+             }*/
+            
+            let arrivalData: [JourneyPlannerArrivalData] = try await journeyPlannerService.fetchLiveArrivalData(stopPlaceID: child.id)
+            
+            for arrival in arrivalData {
+                print(arrival)
+            }
+            
+        } catch EndpointError.networkError(let URLError) {
+            // TODO: Handle this
+        } catch EndpointError.httpError(let statusCode) {
+            // TODO: Handle this
+        } catch EndpointError.decodingError {
+            // TODO: Handle this
+        } catch {
+            // TODO: Handle this
+        }
+    }
+    
+    func fetchArrivalData(for children: [AddFavoritesResult.StopMetadata]) async {
+        do {
+            /*isLoading = true TODO: Create isLoading array for each individual item that can be loaded.
+             
+             defer {
+             isLoading = false
+             }*/
+            for child in children {
+                let arrivalData: [JourneyPlannerArrivalData] = try await journeyPlannerService.fetchLiveArrivalData(stopPlaceID: child.id)
+                
+                for arrival in arrivalData {
+                    print(arrival)
+                }
+            }
+            
+        } catch EndpointError.networkError(let URLError) {
+            // TODO: Handle this
+        } catch EndpointError.httpError(let statusCode) {
+            // TODO: Handle this
+        } catch EndpointError.decodingError {
+            // TODO: Handle this
+        } catch {
+            // TODO: Handle this
+        }
+    }
+    
+    
+     // UpdateArrivalData, when the amount of arrival data goes down to four for a certain nsr stop place, call fetchArrivalData from the point in time of the last arrival data that we have and get 4 queries!
 }
