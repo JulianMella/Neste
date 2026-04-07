316036da52a8d6df61f868e0bc7b45b47e1197d3
Julian Mella <julianmella2@gmail.com>
2026-04-07 16:05:54 +0200

    refactor: conform to favoriteStop struct

--- Neste/UI/Favorites/FavoriteStopView.swift
+++ Neste/UI/Favorites/FavoriteStopView.swift
@@ -8,20 +8,20 @@
 import SwiftUI
 
 struct FavoriteStopView: View {
-    let parent: GeocoderStop
+    let stop: FavoriteStop
     let viewModel: FavoriteStopViewModel
     
     var body: some View {
         VStack(alignment: .leading, spacing: 8) {
-            Text(parent.name)
+            Text(stop.parentStop.name)
                 .font(.system(size: 18))
                 .fontWeight(.semibold)
                 .contextMenu{
                     Button("Delete parent") {
-                        viewModel.deleteParent(parent: parent) //TODO: Better naming
+                        viewModel.deleteParent(parent: stop.parentStop) //TODO: Better naming
                     }
                 }
-            ForEach(viewModel.getChildrenOf(parent), id: \.self) { child in
+            ForEach(viewModel.getChildrenOf(stop.parentStop), id: \.self) { child in
                 HStack {
                     Text(child.publicTransportNumber)
                         .font(.system(size: 16))
@@ -46,7 +46,7 @@ struct FavoriteStopView: View {
                 }
                 .contextMenu {
                     Button("Delete child") {
-                        viewModel.deleteFavorite(parent: parent, child: child) //TODO: Better naming
+                        viewModel.deleteFavorite(parent: stop.parentStop, child: child) //TODO: Better naming
                     }
                 }
             }
