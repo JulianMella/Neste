89bef1702568c87d4b94717a7ef5f3d2c39c77d5
Julian Mella <julianmella2@gmail.com>
2026-04-07 14:40:03 +0200

    feat: fetch arrival data

--- Neste/Entur/JourneyPlanner/JourneyPlannerService.swift
+++ Neste/Entur/JourneyPlanner/JourneyPlannerService.swift
@@ -14,7 +14,7 @@ struct JourneyPlannerEndpoint: GraphQLEndpoint {
 final class JourneyPlannerService {
     let journeyPlannerEndpoint = JourneyPlannerEndpoint()
     
-    func fetchStopData(stopPlaceID: String) async throws -> [JourneyPlannerData] {
+    func fetchStopData(stopPlaceID: String) async throws -> [JourneyPlannerStopMetadata] {
         let query = """
             {
               stopPlace(id: "\(stopPlaceID)") {
@@ -57,12 +57,67 @@ final class JourneyPlannerService {
             throw EndpointError.decodingError(error)
         }
         
-        // TODO: Handle empty list!
+        // TODO: safely unwrap optionals
         return decoded.data.stopPlace.estimatedCalls.map {
-            JourneyPlannerData(
+            JourneyPlannerStopMetadata(
                 publicCode: $0.serviceJourney.journeyPattern.line.publicCode,
                 frontText: $0.destinationDisplay.frontText,
-                transportType: TransportType($0.serviceJourney.transportMode, queryType: .journeyPlanner)!
+                transportType: TransportType($0.serviceJourney.transportMode!, queryType: .journeyPlanner)!
+            )
+        }
+    }
+    
+    func fetchLiveArrivalData(stopPlaceID: String) async throws -> [JourneyPlannerArrivalData]{
+        let query = """
+            {
+              stopPlace(id: "\(stopPlaceID)") {
+                estimatedCalls(
+                  numberOfDeparturesPerLineAndDestinationDisplay: 8
+                  numberOfDepartures: 100
+                  whiteListed: {authorities: ["RUT:Authority:RUT"]}
+                ) {
+                  destinationDisplay {
+                    frontText
+                  }
+                  serviceJourney {
+                    journeyPattern {
+                      line {
+                        publicCode
+                      }
+                    }
+                  }
+                  aimedDepartureTime
+                  expectedDepartureTime
+                }
+              }
+            }
+            """
+        
+        let (data, response): (Data, URLResponse)
+        
+        do {
+            (data, response) = try await URLSession.shared.data(for: journeyPlannerEndpoint.makeRequest(with: query))
+        } catch {
+            throw EndpointError.networkError(error)
+        }
+
+        try journeyPlannerEndpoint.validateHTTPResponse(response)
+        
+        let decoded: GraphQLData<JourneyPlannerResponse>
+        
+        do {
+            decoded = try JSONDecoder().decode(GraphQLData<JourneyPlannerResponse>.self, from: data)
+        } catch {
+            throw EndpointError.decodingError(error)
+        }
+        
+        // TODO: safely unwrap optionals
+        return decoded.data.stopPlace.estimatedCalls.map {
+            JourneyPlannerArrivalData(
+                publicCode: $0.serviceJourney.journeyPattern.line.publicCode,
+                frontText: $0.destinationDisplay.frontText,
+                aimedDepartureTime: $0.aimedDepartureTime!,
+                expectedDepartureTime: $0.expectedDepartureTime!,
             )
         }
     }
