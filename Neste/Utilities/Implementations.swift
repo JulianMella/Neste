c7159d8f87f9cfd785969f2ca8c82f83fccb5da0
Julian Mella <julianmella2@gmail.com>
2026-04-07 21:20:46 +0200

    fix: removed ability to read user selected files

---
+++ Neste/Utilities/Implementations.swift
@@ -0,0 +1,15 @@
+//
+//  Counties.swift
+//  Neste
+//
+//  Created by Julian on 07/04/2026.
+//
+
+static let implementedCounties: [String] = [
+    "Oslo"
+]
+
+
+static let implementedAuthorities = [
+    "
+]
