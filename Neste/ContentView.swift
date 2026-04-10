88afaf8e1e0b960719d4b983dc2baaf53799d823
Julian Mella <julianmella2@gmail.com>
2026-04-10 06:09:06 +0200

    feat: environment based viewmodels

--- Neste/ContentView.swift
+++ Neste/ContentView.swift
@@ -8,15 +8,18 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var favoriteStopViewModel = FavoriteStopViewModel()
+    @State private var stopSearchViewModel = StopSearchViewModel()
     @State private var showStopSearch = false
     
     var body: some View {
         if showStopSearch {
             StopSearchView(showStopSearch: $showStopSearch)
                 .environment(favoriteStopViewModel)
+                .environment(stopSearchViewModel)
         } else {
             BrowseView(showStopSearch: $showStopSearch)
                 .environment(favoriteStopViewModel)
+                .environment(stopSearchViewModel)
         }
     }
 }
